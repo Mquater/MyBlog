851b01f3815bd27e73465f1dff78b83d99f5902f
Mquater <1119008738@qq.com>
2018-06-25 21:30:07 +0800

    4

--- JS/FM.js
+++ JS/FM.js
@@ -1,13 +1,19 @@
-var hxr=new XMLHttpRequest()
-xhr.open('GET','/music.json',true)
-xhr.onload=function(){
-  if(xhr.status>=200 && xhr.status<300 || xhr.status == 304){
-  console.log(Json.Parse(this.requestText))
-  } else {
-  console.log('服务器异常')
+function getMusicList(callback){
+
+  var hxr=new XMLHttpRequest()
+  xhr.open('GET','/music.json',true)
+  xhr.onload=function(){
+    if(xhr.status>=200 && xhr.status<300 || xhr.status == 304){
+     console.log(Json.Parse(this.requestText))
+    } else {
+     console.log('服务器异常')
+    }
+  }
+  xhr.onerror=function(){
+    console.log('服务器异常')
+  }
+  xhr.send()
 }
-}
-xhr.onerror=function(){
-  console.log('服务器异常')
-}
-xhr.send()
+getMusicList(function(list){
+  console.log(list)
+})
