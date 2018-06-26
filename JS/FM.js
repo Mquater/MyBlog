261152447c87831b0ac65ff79870b73dc0a6d015
Mquater <1119008738@qq.com>
2018-06-26 10:55:11 +0800

    6.26

--- JS/FM.js
+++ JS/FM.js
@@ -1,4 +1,6 @@
 var currentIndex=0;
+var clock;
+var musicList=[];
 var audio=new Audio();
 audio.autoplay=true;
 function $(selector){
@@ -6,23 +8,24 @@ function $(selector){
 }
 function getMusicList(callback){
 
-  var hxr=new XMLHttpRequest()
-  xhr.open('GET','/music.json',true)
+  var xhr=new XMLHttpRequest();
+  xhr.open('GET','/music.json',true);
   xhr.onload=function(){
     if(xhr.status>=200 && xhr.status<300 || xhr.status == 304){
-     console.log(Json.Parse(this.requestText));
+     start(JSON.parse(this.requestText));
     } else {
      console.log('获取数据失败');
     }
-  }
+  };
   xhr.onerror=function(){
     console.log('服务器异常');
-  }
-  xhr.send()
+  };
+  xhr.send();
 }
-getMusicList(function(list){
-  console.log(list[currentIndex])
-})
+getMusicList(function start(list){
+  musicList=list;
+  loadMusic(list[currentIndex]);
+});
 function loadMusic(musicObj){
   console.log('begin playing',musicObj);
   $('.musicbox .title').innerText=musicObj.title;
@@ -31,7 +34,7 @@ function loadMusic(musicObj){
 }
 audio.ontimeupdate=function(){
   console.log(this.currentTime);
-}
+};
 audio.onplay=function(){
   clock=setInterval(function(){
     $('.musicbox .progress .progress-current').style.width=(this.currentTime/this.duration) * 100 + '%';
@@ -40,10 +43,10 @@ audio.onplay=function(){
     sec=sec.lenght===2?sec:'0'+sec;
     $('.musicbox .time').innerText=min + sec;
   },1000);
-}
+};
 audio.onpause=function(){
   clearInterval(clock);
-}
+};
 $('.musicbox .play').onclick=function(){
   if(audio.pause){
     audio.play();
@@ -54,4 +57,20 @@ $('.musicbox .play').onclick=function(){
     this.querySelector('iconfont').classlist.remove('icon-play');
     this.querySelector('iconfont').classlist.add('icon-pause');
   }
-}
+};
+$('.musicbox .next').onclick=function(){
+  currentIndex=(++currentIndex)%musicList.lenght;
+  loadMusic(musicList[currentIndex]);
+};
+$('.musicbox .back').onclick=function(){
+    currentIndex=(musicList.length+(--currentIndex))%musicList.lenght;
+    loadMusic(musicList[currentIndex]);
+};
+$('.musicbox .bar').onclick=function(e){
+    var percent=e.offset/parseInt(getComputedStyle(this).width);
+    audio.currentTime=audio.duration * percent;
+};
+audio.onended=function(){
+    currentIndex=(++currentIndex)%musicList.lenght;
+    loadMusic(musicList[currentIndex]);
+};
